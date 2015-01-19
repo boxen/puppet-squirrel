bf0bc01c7d6528b530170114cd82ea05852f5ec0
fundon <cfddream@gmail.com>
2015-01-19 18:49:09 +0800

    upgrade squirrel 0.9.26.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include squirrel
 class squirrel {
   package { 'Squirrel':
-    source   => 'http://dl.bintray.com/lotem/rime/Squirrel-0.9.25.zip',
+    source   => 'http://dl.bintray.com/lotem/rime/Squirrel-0.9.26.2.zip',
     provider => 'compressed_pkg'
   }
 }
--- spec/classes/squirrel_spec.rb
+++ spec/classes/squirrel_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'squirrel' do
   it do
     should contain_package('Squirrel').with({
-      :source   => 'http://dl.bintray.com/lotem/rime/Squirrel-0.9.25.zip',
+      :source   => 'http://dl.bintray.com/lotem/rime/Squirrel-0.9.26.2.zip',
       :provider => 'compressed_pkg'
     })
   end
